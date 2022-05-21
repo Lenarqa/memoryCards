289844ce3240fd9edc366f5abb505191c54cf58c
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2022-05-21 19:16:37 +0700

    feat(card): add pointer down, pointerover, pointerout functions

--- src/card/index.ts
+++ src/card/index.ts
@@ -9,12 +9,30 @@ export const createNewCard = (): Container => {
   card.position.y = 100;
   card.pivot.x = card.height / 2;
   card.pivot.y = card.width / 2;
+  card.interactive = true;
+  card.buttonMode = true;
+
+  card.on('pointerdown', ()=>{
+    console.log("Hello");
+  });
+
+  card.on("pointerover", ()=>{
+    console.log("pointerover")
+    cardBg.tint = 0xffffff;
+  })
+
+  card.on("pointerout", ()=>{
+    console.log("pointerout")
+    cardBg.tint = 0xff0000;
+  })
+
 
   const cardBg: Sprite = new Sprite(Texture.WHITE);
   cardBg.anchor.set(0.5);
   cardBg.width = 100;
   cardBg.height = 150;
   cardBg.tint = 0xff0000;
+ 
 
   const cardText: Text = new Text("Hello №1", {fontSize: 30, wordWrap: true, align: "center"});
   cardText.anchor.set(0.5);
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
-import { Application, Text, Container, Sprite, Texture} from 'pixi.js'
-import { createNewCard } from './card';
+import { Application, Text, Loader } from "pixi.js";
+import { createNewCard } from "./card";
 
 const app = new Application({
-	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
-	resolution: window.devicePixelRatio || 1,
-	autoDensity: true,
-	backgroundColor: 0x6495ed,
-	width: innerWidth,
-	height: innerHeight
+  view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
+  resolution: window.devicePixelRatio || 1,
+  autoDensity: true,
+  backgroundColor: 0x6495ed,
+  width: innerWidth,
+  height: innerHeight,
 });
 
 const helloText: Text = new Text("Hello world!");
 helloText.anchor.set(0.5);
-// helloText.x = app.screen.width / 2;
-// helloText.y = app.screen.height / 2;
 
 const card = createNewCard();
 app.stage.addChild(card);
